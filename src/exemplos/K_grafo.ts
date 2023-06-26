6e8df27e370fa4095c0843434c63aeba26597be4
JoseClenildo <85517137+JoseClenildo@users.noreply.github.com>
2023-06-26 18:57:26 +0000

    qualquer coisa

---
+++ src/exemplos/K_grafo.ts
@@ -0,0 +1,26 @@
+import { GrafoLib } from "../models/GrafoLib";
+import * as readline from 'readline';
+
+export default function kGrafo(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Insira a quantidade de vértices desejados ', (answer)=> {
+        const kgrafo = new GrafoLib;
+        var counter;
+        var destino;
+        var resposta = parseInt(answer);
+        for(counter = 0; counter < resposta; counter++){
+            kgrafo.adicionarVertice(counter);
+        }
+        for(counter = 0; counter < resposta; counter++){
+            for(destino = counter + 1; destino < resposta; destino++){
+                kgrafo.adicionarAresta(counter, destino);
+            }
+        }
+        kgrafo.imprimirGrafo();
+        rl.close();
+    });
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Grafo1 from "./exemplos/Grafo1";
 import Grafo2 from "./exemplos/Grafo2";
-
+import kGrafo from "./exemplos/K_grafo";
+    
 console.log("Grafo 1\n")
 Grafo1()
 console.log("\n")
 console.log("Grafo 2\n")
 Grafo2()
+
+console.log("K grafo\n");
+kGrafo();
